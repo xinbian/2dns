13e8193bf0cd38d7a7fffe2433903aae45dc8dd9
Xin Bian <bianxin2006@gmail.com>
2016-12-10 16:00:49 -0500

    intial for mpi

--- python_2d_ns/python_2d_ns.py
+++ python_2d_ns/python_2d_ns.py
@@ -37,112 +37,154 @@ k_f=2;          #for Taylor-Green forcing
 
 diag_out_step = 5000; #step frequency of outputting diagnostics
 
+#------------MPI setup---------
+comm = MPI.COMM_WORLD
+num_processes = comm.Get_size()
+rank = comm.Get_rank()
+#slab decomposition, split in x in physical space, in y in Fourrier space
+Np = Nx/num_processes
 
+#---------modifited 2d FFT and IFFT
+#def ifftn_mpi(fu, u):
+#    Uc_hat[:]=ifft(fu , axis=0)
+#    comm.Alltoall([Uc_hat, MPI.DOUBLE_COMPLEX], [U_mpi, MPI.DOUBLE_COMPLEX])
+#    Uc_hatT[:] = rollaxis(U_mpi, 1).reshape(Uc_hatT.shape)
+#    u[:]= ifft (Uc_hatT, axis =1)
+#    return u
+#def fftn_mpi(u, fu):
+#    Uc_hatT[:] = fft(u, axis=1)
+#    U_mpi[:] = rollaxis(Uc_hatT.reshape(Np, num_processes, Np), 1)
+#    comm.Alltoall([U_mpi, MPI.DOUBLE_COMPLEX], [fu, MPI.DOUBLE_COMPLEX])
+#    fu[:]= fft(fu, axis=0)
+#    return fu  
+#
 #-----------GRID setup-----------
 Lx=2*pi;
 Ly=2*pi;
 dx=Lx/Nx;
 dy=Ly/Ny;
-x=zeros((Nx, Ny), dtype=float);
-y=zeros((Nx, Ny), dtype=float);
-kx=zeros((Nx, Ny), dtype=float);
-ky=zeros((Nx, Ny), dtype=float);
+x=zeros((Np, Ny), dtype=float);
+y=zeros((Np, Ny), dtype=float);
+kx=zeros((Nx, Np), dtype=float);
+ky=zeros((Nx, Np), dtype=float);
+#initilize x space coordinate
 for j in range(Ny):
-    kx[0:Nx,j]=range(-Nx/2, Nx/2);
-    x[0:Nx,j] =range(-Nx/2, Nx/2);
-x=x*dx;
-for i in range(Nx):
-    ky[i,0:Ny]=range(-Ny/2, Ny/2);
+    x[0:Np,j]=range(Np);
+x=x-(num_processes/2-rank)*Np
+#x=x*dx;
+
+for i in range(Np):
     y[i,0:Ny] =range(-Ny/2, Ny/2);
-y=y*dy;
+#y=y*dy;
+
+for j in range(Np):
+    kx[0:Nx,j]=range(-Nx/2, Nx/2);
+for i in range(Nx):
+    ky[i,0:Np]=range(Np);
+ky=ky-(num_processes/2-rank)*Np
+
 k2=kx**2+ky**2;
 for i in range(Nx):
-    for j in range(Ny):
+    for j in range(Np):
          if(k2[i,j] == 0):
              k2[i,j]=1e-5; #so that I do not divide by 0 below when using projection operator
 
 k2_exp=exp(-nu*(k2**5)*dt-nu_hypo*dt);
 
+#---------dealiasing function----
+def delias():
+    for i in range(Nx):
+        for j in range(Np):
+            if(sqrt(k2[i,j]) >= Nx/3.):
+                Vxhat[i,j]=0;
+                Vyhat[i,j]=0;
+    #------Projection operator on velocity fields to make them solenoidal-----
+    tmp = (kx*Vxhat + ky*Vyhat)/k2;
+    Vxhat = Vxhat - kx*tmp;
+    Vyhat = Vyhat - ky*tmp;
+    return Vxhat, Vyhat
+
+
 #-----------Force setup----------
-Fxhat = zeros((Nx, Ny), dtype=complex);
-Fyhat = zeros((Nx, Ny), dtype=complex);
-for iss in [-1, 1]:
-    for jss in [-1, 1]:
-        for i in range(Nx):
-            for j in range(Ny):
-                if(int(kx[i,j])==iss*k_f and int(ky[i,j])==jss*k_f):
-                    Fxhat[i,j] = -1j*iss;
-                    Fyhat[i,j] = -1j*(-jss);
-                
-Fxhat=5e-2*Fxhat;     
-Fyhat=5e-2*Fyhat;
-
-#assume no foricng
-Fxhat=0.0*Fxhat
-Fyhat=0.0*Fyhat
-
+#Fxhat = zeros((Nx, Np), dtype=complex);
+#Fyhat = zeros((Nx, Np), dtype=complex);
+#for iss in [-1, 1]:
+#    for jss in [-1, 1]:
+#        for i in range(Nx):
+#            for j in range(Ny):
+#                if(int(kx[i,j])==iss*k_f and int(ky[i,j])==jss*k_f):
+#                    Fxhat[i,j] = -1j*iss;
+#                    Fyhat[i,j] = -1j*(-jss);
+#                
+#Fxhat=5e-2*Fxhat;     
+#Fyhat=5e-2*Fyhat;
+#
+##assume no foricng
+#Fxhat=0.0*Fxhat
+#Fyhat=0.0*Fyhat
+#
 
 #-----------Variables------------
-Vxhat=zeros((Nx, Ny), dtype=complex);
-Vyhat=zeros((Nx, Ny), dtype=complex);
-Wzhat=zeros((Nx, Ny), dtype=complex);
-Vx=zeros((Nx, Ny), dtype=float);
-Vy=zeros((Nx, Ny), dtype=float);
-Wz=zeros((Nx, Ny), dtype=float);
+Vxhat=zeros((Nx, Np), dtype=complex);
+Vyhat=zeros((Nx, Np), dtype=complex);
+Wzhat=zeros((Nx, Np), dtype=complex);
+Vx=zeros((Np, Ny), dtype=float);
+Vy=zeros((Np, Ny), dtype=float);
+Wz=zeros((Np, Ny), dtype=float);
 
 
-#----Initialize: Taylor-Green Velocity in Fourier space-----------
+##----Initialize: Taylor-Green Velocity in Fourier space-----------
 if (new==1):
-    Vxhat = zeros((Nx, Ny), dtype=complex);
-    Vyhat = zeros((Nx, Ny), dtype=complex);
-    for iss in [-1, 1]:
-        for jss in [-1, 1]:
-            for i in range(Nx):
-                for j in range(Ny):
-                    if(int(kx[i,j])==iss*k_ic and int(ky[i,j])==jss*k_ic):
-                        Vxhat[i,j] = -1j*iss;
-                        Vyhat[i,j] = -1j*(-jss);
-                
-#Set total energy to 1
-    Vxhat=0.5*Vxhat;     
-    Vyhat=0.5*Vyhat;
-    
-    Vx=10*random.rand(Nx,Ny)
-    Vy=10*random.rand(Nx,Ny)
-    
+#    Vxhat = zeros((Nx, Ny), dtype=complex);
+#    Vyhat = zeros((Nx, Ny), dtype=complex);
+#    for iss in [-1, 1]:
+#        for jss in [-1, 1]:
+#            for i in range(Nx):
+#                for j in range(Ny):
+#                    if(int(kx[i,j])==iss*k_ic and int(ky[i,j])==jss*k_ic):
+#                        Vxhat[i,j] = -1j*iss;
+#                        Vyhat[i,j] = -1j*(-jss);
+#                
+##Set total energy to 1
+#    Vxhat=0.5*Vxhat;     
+#    Vyhat=0.5*Vyhat;
+#    
+    Vx=10*random.rand(Np,Ny)
+    Vy=10*random.rand(Np,Ny)
+#    
     Vxhat = (fftshift(fft2(ifftshift(Vx))));
     Vyhat = (fftshift(fft2(ifftshift(Vy))));
-    
-#----read from data---------
+#    
+##----read from data---------
 else:
     Vxhat = Vxhat_restart;
     Vyhat = Vyhat_restart;
-
-
-#------Dealiasing------------------------------------------------
+#
+#
+##------Dealiasing------------------------------------------------
 for i in range(Nx):
-      for j in range(Ny):
+     for j in range(Np):
           if(sqrt(k2[i,j]) >= Nx/3.):
               #print k2[i,j]
               Vxhat[i,j]=0;
               Vyhat[i,j]=0;
-
-
+#
+#
 #------Projection operator on velocity fields to make them solenoidal-----
 tmp = (kx*Vxhat + ky*Vyhat)/k2;
 Vxhat = Vxhat - kx*tmp;
 Vyhat = Vyhat - ky*tmp;
-
+#
 #------Storing variables for later use in time integration--------
 Vxhat_t0 = Vxhat;
 Vyhat_t0 = Vyhat;
-
+#
 #----Main Loop-----------
 for istep in range(Nstep):
     #------Dealiasing------------------------------------------------
     for i in range(Nx):
-        for j in range(Ny):
-            if(np.sqrt(k2[i,j]) >= Nx/3.):
+        for j in range(Np):
+            if(sqrt(k2[i,j]) >= Nx/3.):
                 Vxhat[i,j]=0;
                 Vyhat[i,j]=0;
     #Projection operator on velocity
@@ -170,12 +212,10 @@ for istep in range(Nstep):
     #move non-linear term back to Fourier k-space
     NLxhat = (fftshift(fft2(ifftshift(NLx))));
     NLyhat = (fftshift(fft2(ifftshift(NLy))));
- 
-
     
     #------Dealiasing------------------------------------------------
     for i in range(Nx):
-        for j in range(Ny):
+        for j in range(Np):
             if(sqrt(k2[i,j]) >= Nx/3.):
                 NLxhat[i,j]=0;
                 NLyhat[i,j]=0;
@@ -188,14 +228,14 @@ for istep in range(Nstep):
     #Integrate in time
     #---Euler for 1/2-step-----------
     if(istep==0):
-          Vxhat = Vxhat + dt_h*(NLxhat -nu*(k2**5)*Vxhat -nu_hypo*(k2**(-0))**Vxhat) + dt_h**Fxhat;
-          Vyhat = Vyhat + dt_h*(NLyhat -nu*(k2**5)*Vyhat -nu_hypo*(k2**(-0))**Vyhat) + dt_h**Fyhat;
+          Vxhat = Vxhat + dt_h*(NLxhat -nu*(k2**5)*Vxhat -nu_hypo*(k2**(-0))**Vxhat);
+          Vyhat = Vyhat + dt_h*(NLyhat -nu*(k2**5)*Vyhat -nu_hypo*(k2**(-0))**Vyhat);
           oNLxhat = NLxhat;
           oNLyhat = NLyhat;
     #---Midpoint time-integration----
     elif(istep==1):
-          Vxhat = Vxhat_t0 + dt*(NLxhat -nu*(k2**5)*Vxhat -nu_hypo*(k2**(-0))*Vxhat) + dt*Fxhat;
-          Vyhat = Vyhat_t0 + dt*(NLyhat -nu*(k2**5)*Vyhat -nu_hypo*(k2**(-0))*Vyhat) + dt*Fyhat;          
+          Vxhat = Vxhat_t0 + dt*(NLxhat -nu*(k2**5)*Vxhat -nu_hypo*(k2**(-0))*Vxhat);
+          Vyhat = Vyhat_t0 + dt*(NLyhat -nu*(k2**5)*Vyhat -nu_hypo*(k2**(-0))*Vyhat);          
     #---Adam-Bashforth integration---
     else:
           Vxhat = Vxhat + dt*(1.5*NLxhat - 0.5*oNLxhat*k2_exp);
